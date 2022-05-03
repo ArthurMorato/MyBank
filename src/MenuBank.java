f1efa452d678c5ef3b8013fcd92fbaacb15dcbba
ArthurMorato <84877822+ArthurMorato@users.noreply.github.com>
2022-05-03 21:25:00 +0100

    bem vindo

---
+++ src/MenuBank.java
@@ -0,0 +1,10 @@
+public class MenuBank {
+    public static void main(String[] args) {
+    }
+
+    public MenuBank() {
+        System.out.println("" +
+                "[][][]MENU BANK[][][]" +
+                "Bem vindo ao MyBank! Crie sua conta.");
+    }
+}
